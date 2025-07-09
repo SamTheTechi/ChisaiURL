44be7f68224ffb50cea6ffca1ab60059bac47b24
SamTheProBot <gsameer478@gmail.com>
2025-07-10 00:37:32 +0530

    added redis for faster lookups

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import { fileURLToPath } from "url";
-import path from 'path';
-import cors from 'cors';
-import { URL } from './model.js';
-import { nanoid } from 'nanoid';
+import { URL } from "./model.js";
+import { nanoid } from "nanoid";
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import path from "path";
+import cors from "cors";
+import Redis from "ioredis";
 
 dotenv.config();
+const redis = new Redis(process.env.REDIS_KEY);
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -20,33 +22,79 @@ app.use(express.static(path.join(__dirname, "public")));
 const port = parseInt(process.env.PORT || process.env.LOCALPORT || "5000", 10);
 
 app.post("/chisai", async (req, res) => {
-  const { originalUrl } = req.body;
-  console.log(`Received data: ${originalUrl}`);
+  const originalUrl = req.body.originalUrl;
+
   try {
-    const doExist = await URL.findOne({ original: originalUrl });
-    if (!doExist) {
-      const sid = nanoid(7);
-      await URL.create({
-        original: originalUrl,
-        short: sid,
-      });
-      res.status(200).json({ url: sid, msg: `New short URL created!` });
-    } else {
-      res.status(200).json({ url: doExist.short, msg: `Short URL already exists.` });
+    const key = `chisai_original_short:${originalUrl}`;
+
+    // hit cache first
+    const cached = await redis.get(key);
+    if (cached) {
+      console.log("hit redis");
+      return res
+        .status(200)
+        .json({ url: cached, msg: `Short URL already exists.` });
     }
+
+    // check db if the url already existes or not
+    const exists = await URL.findOne({ original: originalUrl });
+    if (exists) {
+      // save into cache
+      await redis.set(key, exists.short);
+
+      return res
+        .status(200)
+        .json({ url: exists.short, msg: `Short URL already exists.` });
+    }
+
+    // create new instance
+    const uid = nanoid(7);
+    await URL.create({
+      original: originalUrl,
+      short: uid,
+    });
+
+    // save into cache
+    await redis.set(key, uid);
+
+    return res.status(200).json({ url: uid, msg: `New short URL created!` });
   } catch (e) {
-    res.status(401).json({ url: `Error occurred`, msg: `Unexpected error: ${e}` });
+    res
+      .status(401)
+      .json({ url: `Error occurred`, msg: `Unexpected error: ${e}` });
   }
 });
 
 app.get("/:short", async (req, res) => {
+  const param = req.params.short;
+
+  if (!param) {
+    res.status(400).json({ msg: `Provide params` });
+  }
+
   try {
-    const url = await URL.findOne({ short: req.params.short });
-    console.log(`Found URL: ${url}`);
-    if (url) return res.redirect(url.original);
-    res.status(404).json({ msg: `URL not found` });
+    const key = `chisai_short_original:${param}`;
+
+    // try redis cache
+    const cached = await redis.get(key);
+    if (cached) {
+      console.log("hit redis");
+      return res.redirect(cached);
+    }
+
+    // fallback to DB
+    const url = await URL.findOne({ short: param });
+    if (!url) {
+      return res.status(404).json({ msg: `URL not found` });
+    }
+
+    // cache it for furture requests (for 2 days)
+    await redis.set(key, url.original, "EX", 60 * 60 * 24);
+
+    return res.redirect(url.original);
   } catch (e) {
-    res.status(500).json({ msg: `Server error: ${e}` });
+    console.error(e);
+    return res.status(500).json({ msg: `Server error: ${e}` });
   }
 });
 
@@ -56,10 +104,11 @@ app.get("/", (_, res) => {
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL), {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
+    await mongoose.connect(process.env.MONGODB_URL),
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      };
     app.listen(port, () => {
       console.log(`Server is running at http://localhost:${port}`);
     });
@@ -69,4 +118,3 @@ const start = async () => {
 };
 
 start();
-
